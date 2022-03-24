8f6539b72eb9fd83830e1ba7b7a23341fb23e6f9
charenkumar <102220623+charenkumar@users.noreply.github.com>
2022-03-25 00:34:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
     res.send("<h1>Hi world, This is Charen...</h1>");
 });
 
-app.listen(3000, () => {
-    console.log("App running on port 3000...");
+app.listen(8000, () => {
+    console.log("App running on port 8000...");
 });
