a4324ec3521477aa6e9ab4ac406e82110f1d39de
charenkumar <102220623+charenkumar@users.noreply.github.com>
2022-03-27 01:08:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hi world, This is Charen...</h1>");
+  res.send("<h1>Hello world this is charen...</h1>");
 });
 
-app.listen(8000, () => {
-    console.log("App running on port 8000...");
+app.listen(3000, () => {
+  console.log("App running on port 3000...");
 });
